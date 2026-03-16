34864593adedb32c500562ba3f2500bb708fc70f
indsf <132574423+indsf@users.noreply.github.com>
2026-03-16 14:44:22 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,39 +1,40 @@
 #include <iostream>
 using namespace std;
-long long a[101][10] = { 0 };
+
+int a[101][10] = {0};
+int n;
 int mod = 1000000000;
-long long ans = 0;
-void solve(int n)
+void solve()
 {
-	for (int i = 1; i <= 9; i++)
-		a[0][i] = 1;
+    cin >> n;
+    for(int i = 1;i<=9;i++)
+        a[0][i] = 1;
 
-	for (int i = 1; i <= n; i++)
-	{
-		a[i][0] = a[i - 1][1];
-		a[i][9] = a[i - 1][8];
-		for (int j = 1; j < 9;j++)
-		{
-			a[i][j] = (a[i - 1][j - 1] + a[i - 1][j + 1]) % mod;
-		}
-	}
-	
-	for (int i = 0; i < 10; i++)
-	{
-		ans = (ans + a[n - 1][i]) % mod;
-	}
+    int ans = 0;
+    for(int i = 1;i<=n;i++)
+    {
+        a[i][0] = a[i-1][1];
+        a[i][9] = a[i-1][8];
+        for(int j = 1;j<9;j++)
+        {
+            a[i][j] = (a[i-1][j-1] + a[i-1][j+1]) % mod;
+        }
+    }
+
+    for(int i = 0;i<10;i++)
+    {
+        ans = (ans + a[n-1][i]) % mod;
+    }
+    cout << ans << '\n';
+
 }
-
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	
-	int n;
-	cin >> n;
-	solve(n);
-	cout << ans << '\n';
-	return 0;
-}
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
+    solve();
+
+    return 0;
+}
