87b387d04cc35ecc23cb6b9045ac4af20ef8a290
indsf <132574423+indsf@users.noreply.github.com>
2026-03-17 12:27:04 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void solve(int n)
-{    
-    
+int n;
+
+void solve()
+{
     for(int i = 2;i*i<=n;i++)
     {
         while(n%i == 0)
-        {
+        {            
             cout << i << '\n';
-            n /= i;
-        }
+            n /= i;    
+        }        
     }
     if(n != 1)
         cout << n << '\n';
-
 }
 
 int main()
@@ -22,8 +22,10 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
     cin >> n;
-    solve(n);
+
+    solve();
+
+
     return 0;
 }
